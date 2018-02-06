bea7bc90b5408ac3da2a532011879edc794d073c
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-02-06 09:35:24 -0600

    icelandic

---
+++ js/icelandic.js
@@ -0,0 +1,18 @@
+"use strict;";
+
+function icelandic() {
+
+    var icelandic = {
+        "merry": "gleðilegt",
+        "christmas": "jól",
+        "and": "og",
+        "happy": "farsælt",
+        "new": "komandi",
+        "year": "ár"
+    };
+
+    return icelandic;
+
+}
+
+module.exports = icelandic;
