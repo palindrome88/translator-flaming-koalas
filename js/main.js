40d0904e3ca6ca8de134a86002ee60ded663d706
Ben Atkins <benjatkins13@gmail.com>
2018-02-06 09:34:24 -0600

    ba-changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 "use strict";
 
+var transButton = document.getElementById("translate")
+
 console.log("test");
 
 let lexicon = require("/.lexicon");
 let spanish = require("/.spanish");
 let czech = require("/.czech");
 let french = require("/.french");
+
+
+
+function translate (){
+    if (document.getElementById("spanish").checked){
+        return outputSpanish
+    }
+    if (document.getElementById("czech").checked){
+        return outputCzech
+    }
+    if (document.getElementById("french").checked){
+        return outputFrench
+    }
+
+}
+
+
