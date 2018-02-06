d68408d77c6dbe9cb310b6596bee3f261c89cc7f
Ben Atkins <benjatkins13@gmail.com>
2018-02-06 12:15:08 -0600

    no changes

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-
 "use strict";
 function czech() {
 
@@ -76,7 +75,7 @@ let Languages = {
 
 
 function lexicon() {
-
+console.log("lex works");
 	var greeting = "merry christmas and a happy new year"; // INPUT FROM CONSOLE HERE
 	var sentence, newsentence, newsentence2, newsentence3, newsentence4 = []; // newsentence## input values to be translated
 	
@@ -116,6 +115,7 @@ function lexicon() {
 		
 		}
 	}
+
 	var output_czech = newsentence.join(" ");
 	var output_french = newsentence2.join(" ");
 	var output_spanish = newsentence3.join(" ");
