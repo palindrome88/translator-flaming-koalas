19c5974c86ad918a732be8363ac46f63e6dfb7f5
Ben Atkins <benjatkins13@gmail.com>
2018-02-06 09:02:04 -0600

    no changes

--- js/spanish.js
+++ js/spanish.js
@@ -2,34 +2,50 @@ var spanishLex = {
     "merry":"feliz",
     "christmas":"Navidad",
     "and":"y",
-    "happy":"gott",
-    "new":"nytt",
-    "ear":"år"
+    "happy":"feliz",
+    "new":"nuevo",
+    "year":"ano"
 }
 
-console.log(Object.keys(spanishLex(2)))
-// transButton = document.getElementById("translate");
-// greeting = ["merry","christmas","and", "a", "happy", "new", "year"]
+propNames = (Object.getOwnPropertyNames(spanishLex));
+
+// console.log(propNames)
+
+transButton = document.getElementById("translate");
+
+
+greeting = ["merry","christmas","and", "happy", "new", "year","merry", "merry", "merry"]
+
+
 // console.log(spanishLex["merry"])
-// translatedGreeting= []
-// function translateToSpanish (){
-// if (document.getElementById("spanish").checked){
-//     for(i=0;i<greeting.length;i++){
-//         var currentWord = greeting[i];
-//         console.log(currentWord);
-//         for (i=0;i<spanishLex.length;i++){
-//             compareWord = Object.spanishLex["i"];
-//             console.log(compareWord);
-//             if(currentWord === compareWord){
-//                 translatedGreeting.push(spanishLex.merry)
-//             }
-//         }
+
+
+translatedGreeting= []
+
+
+function translateToSpanish (){
+if (document.getElementById("spanish").checked){
+    console.log("works");
+    console.log(propNames);
+    for(i=0;i<greeting.length;i++){
+        var currentWord = greeting[i];
+        // console.log("current word",currentWord);
+        for(j=0;j<propNames.length;j++){
+            var compareWord = propNames[j];
+            // console.log("compare word", compareWord)
+            if(currentWord === compareWord){
+                x = spanishLex[compareWord]
+                // console.log("push", x)
+                translatedGreeting.push(x);
+
+            }
+        }
             
-//     }
+    }
+    console.log(translatedGreeting)
+}
+}
 
 
 
-// ;
-
-// transButton.addEventListener("click",translateToSpanish);
-
+transButton.addEventListener("click",translateToSpanish);
